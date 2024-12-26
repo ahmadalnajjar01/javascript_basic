e47b5423150fd2233ff2350a8266594f805103c9
ahmadalnajjar01 <ahmadalnajjar609@gmail.com>
2024-12-26 16:14:17 +0300

    done

---
+++ main.js
@@ -0,0 +1,47 @@
+// Task 1
+
+let x = 10.2;
+console.log(-53);
+console.log("JavaScript" + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4 === "4");
+console.log(4 != 5);
+console.log(7 <= 8);
+console.log("Hello" + 5);
+console.log(Math.ceil(x) - Math.floor(x));
+console.log(Math.pow(x, 2));
+
+// Task 2
+
+// let number = prompt("Enter Your Number");
+
+// alert("Your Number Is " + number);
+
+// Task 3
+
+// let num1 = prompt("Enter First Number");
+// let num2 = prompt("Enter Second Number");
+
+// let sum = +num1 + +num2;
+// console.log(sum);
+
+// Task 4
+
+// let n1 = prompt("Enter First Number");
+// let n2 = prompt("Enter Second Number");
+
+// let product = n1 * n2;
+// alert("Your Number Is " + product);
+
+// Task 5
+
+// let n1 = prompt("Enter First Number");
+// let n2 = prompt("Enter Second Number");
+
+// let result = n1 / n2;
+// console.log("The result of dividing " + n1 + " by " + n2 + " is: " + result);
+
+// let sum = +n1 + +n2;
+// console.log(sum);
